fe3e58993e278e0a539001203f53b87f44d6168c
salusKarabu <k.mariasalus@gmail.com>
2023-08-19 02:37:41 -0500

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
-
-
-======================
-
-6-print_comb3.py
-
-#!/usr/bin/python3
 # 6-print_comb3.py
 
 """Print all possible different combinations of two digits in ascending order.
